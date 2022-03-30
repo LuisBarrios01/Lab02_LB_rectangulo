699e3a128786a3df00afcfca4b29d90d5eccc7e8
Luis Barrios <luis.barrios.p@tecsup.edu.pe>
2022-03-30 16:03:18 -0500

    Agregar archivos de proyecto.

---
+++ Lab02_LB/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab02_LB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            int y;
+            double perimetro;
+            double area;
+            Console.WriteLine("Ingrese un Valor para X:");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese un Valor para Y:");
+            y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("El lado X es: " + x);
+            Console.WriteLine("El lado Y es: " + y);
+            perimetro = (2 * (x + y));
+            area = x * y;
+            Console.WriteLine("El Perimetro es: " + perimetro);
+            Console.WriteLine("El Area es: " + area);
+            Console.Read();
+        }
+    }
+}
